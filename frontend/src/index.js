ab7d63bfd458f40bb28214c6ae02e23b306a7f98
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-04-15 15:03:04 +0800

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,17 @@ import './index.css'
 import App from './App'
 import { LeadsContextProvider } from './context/LeadsContext'
 import { AuthContextProvider } from './context/AuthContext'
+import { UsersContextProvider } from './context/UsersContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <LeadsContextProvider>
-            <App />
-      </LeadsContextProvider>
+      <UsersContextProvider>
+        <LeadsContextProvider>
+              <App />
+        </LeadsContextProvider>
+      </UsersContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 )
