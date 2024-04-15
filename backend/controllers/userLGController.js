afe8322792e6065bc31db376cab5a46f37854bf5
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-04-15 14:53:02 +0800

    updated

--- backend/controllers/userLGController.js
+++ backend/controllers/userLGController.js
@@ -1,5 +1,6 @@
 const UserLG = require('../models/userLGmodel')
 const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 
 const createToken = (_id) => {
     return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
@@ -25,17 +26,17 @@ const loginUserLG = async (req, res) => {
 
 // signup user
 const signupUserLG = async (req, res) => {
-  const {name, email, password, role} = req.body
+  const {name, email, password, role, birthday, number, homeaddress, gender} = req.body
 
   try {
-    const userLG = await UserLG.signup(name, email, password, role)
+    const userLG = await UserLG.signup(name, email, password, role, birthday, number, homeaddress, gender)
     userLG.isActive = true // Mark user as active
     await userLG.save() // Save the updated user status
 
     // create a token
     const token = createToken(userLG._id)
 
-    res.status(200).json({name, email, token, role})
+    res.status(200).json({name, email, token, role, birthday, number, homeaddress, gender})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
@@ -54,4 +55,66 @@ const logoutUserLG = async (req, res) => {
 }
 }
 
-module.exports = { signupUserLG, loginUserLG, logoutUserLG }
+// get all users
+const getUserLG = async (req, res) => {
+
+  const userlgs = await UserLG.find({}).sort({createdAt: -1})
+
+  res.status(200).json(userlgs)
+}
+
+// get a single user
+const getSingleUserLG = async (req, res) => {
+  const { id } = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(404).json({error: 'No user found'})
+  }
+
+  const userlg = await UserLG.findById(id)
+
+  if (!userlg) {
+      return res.status(404).json({error: "No user found"})
+  }
+
+  res.status(200).json(userlg)
+}
+
+// update userLG
+const updateUserLG = async (req, res) => {
+  const { id } = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(404).json({error: 'No user found'})
+  }
+
+  const userlg = await UserLG.findOneAndUpdate({_id: id}, {
+      ...req.body
+  })
+
+  if(!userlg) {
+      return res.status(400).json({error: 'No user found'})
+  }
+
+  res.status(200).json(userlg)
+}
+
+// delete user
+const deleteUserLG = async (req, res) => {
+  const { id } = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(404).json({error: 'No user found'})
+  }
+
+  const userlg = await UserLG.findOneAndDelete({_id: id})
+
+  if(!userlg) {
+      return res.status(400).json({error: 'No user found'})
+  }
+
+  res.status(200).json(userlg)
+
+}
+
+module.exports = { signupUserLG, loginUserLG, logoutUserLG, updateUserLG, getUserLG, getSingleUserLG, deleteUserLG }
